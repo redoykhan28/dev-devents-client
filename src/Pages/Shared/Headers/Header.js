3ac6f0f4415d3b3cd0a8b0d733bf885cb4b0e09f
unknown <redoyk2015@gmail.com>
2022-11-08 05:48:53 +0600

    profile handling done

--- src/Pages/Shared/Headers/Header.js
+++ src/Pages/Shared/Headers/Header.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { authContext } from '../../../Context/AuthContext';
 import logo from '../../../images/Capture.PNG'
 import './Header.css'
 
 const Header = () => {
+
+    //use context
+    const { user, logout } = useContext(authContext)
+
+    //logout 
+    const handleLogout = () => {
+
+        logout()
+            .then(res => { })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-white fixed-top">
@@ -13,6 +26,11 @@ const Header = () => {
 
                         <Link className='navbar-brand"'><img src={logo} className='img-fluid logo' alt="logo" /></Link>
 
+
+                    </div>
+
+
+                    <div className="third-part d-none d-lg-flex">
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
@@ -24,25 +42,45 @@ const Header = () => {
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className='d-flex flex-column flex-lg-row'>
 
-                                    <NavLink to={'/home'} className={({ isActive }) => isActive ? 'clr mx-2 text-decoration-none fw-semibold' : 'mx-2 text-decoration-none text-black fw-semibold'}>Home</NavLink>
+                                    <NavLink to={'/home'} className={({ isActive }) => isActive ? 'clr me-4 text-decoration-none fw-semibold' : 'me-2 text-decoration-none text-black fw-semibold'}>Home</NavLink>
 
-                                    <NavLink to={'/services'} className={({ isActive }) => isActive ? 'clr mx-2 text-decoration-none fw-bold' : 'mx-2 text-decoration-none text-black fw-semibold'}>Services</NavLink>
+                                    <NavLink to={'/services'} className={({ isActive }) => isActive ? 'clr mx-4 text-decoration-none fw-bold' : 'mx-4 text-decoration-none text-black fw-semibold'}>Services</NavLink>
 
-                                    <NavLink to={'/blogs'} className={({ isActive }) => isActive ? 'clr mx-2 text-decoration-none fw-bold' : 'mx-2 text-decoration-none text-black fw-semibold'}>Blogs</NavLink>
+                                    <NavLink to={'/blogs'} className={({ isActive }) => isActive ? 'clr mx-4 text-decoration-none fw-bold' : 'mx-4 text-decoration-none text-black fw-semibold'}>Blogs</NavLink>
 
-                                    <NavLink to={'/add-service'} className={({ isActive }) => isActive ? 'clr mx-2 text-decoration-none fw-bold' : 'mx-2 text-decoration-none text-black fw-semibold'}>Add Service</NavLink>
+                                    {
+                                        user ?
+                                            <>
+                                                <NavLink to={'/add-service'} className={({ isActive }) => isActive ? 'clr mx-4 text-decoration-none fw-bold' : 'mx-4 text-decoration-none text-black fw-semibold'}>Add Service</NavLink>
 
-                                    <Link to={'/login'} className="btn loginbtn my-2 my-lg-0 d-lg-none d-block" role="button" aria-disabled="true">Login</Link>
+                                                <NavLink to={'/add-service'} className={({ isActive }) => isActive ? 'clr mx-4 text-decoration-none fw-bold' : 'mx-4 text-decoration-none text-black fw-semibold'}>My Review</NavLink></>
+                                            :
+                                            undefined
+                                    }
+
+                                    {
+                                        user ?
+                                            <Link onClick={handleLogout} className="btn logoutbtn my-2 my-lg-0 d-lg-none d-block" role="button" aria-disabled="true">Logout</Link>
+                                            :
+                                            <Link to={'/login'} className="btn loginbtn my-2 my-lg-0 d-lg-none d-block" role="button" aria-disabled="true">Login</Link>
+                                    }
                                 </li>
                             </ul>
 
                         </div>
-                    </div>
 
+                        {
+                            user ?
+                                <div className='d-flex align-items-center'>
+                                    <div className=''>
+                                        <img className='img-fluid dp shadow-lg rounded-circle ms-4 me-2' src={user.photoURL} alt="Dp" />
+                                    </div>
+                                    <Link onClick={handleLogout} className=" logoutbtn ms-2" role="button" aria-disabled="true">Logout</Link>
+                                </div>
 
-                    <div className="third-part d-none d-lg-flex">
-
-                        <Link to={'/login'} className=" loginbtn" role="button" aria-disabled="true">Login</Link>
+                                :
+                                <Link to={'/login'} className=" loginbtn ms-5" role="button" aria-disabled="true">Login</Link>
+                        }
 
                     </div>
 
